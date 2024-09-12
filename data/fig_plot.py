af56b1f4c4852c276b436a8ba8d68b670690f51c
shakiritis <shakiritiz@gmail.com>
2024-09-12 10:44:56 -0400

    whatever

--- data/fig_plot.py
+++ data/fig_plot.py
@@ -2,6 +2,7 @@ import os
 import re
 import random
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from glob import glob
@@ -18,17 +19,19 @@ N = 1000
 
 plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 15
-
+# plt.rcParams["text.usetex"] =True
+# params = {'mathtext.default': 'regular' }          
+# plt.rcParams.update(params)
 Pmax = [0.79, 0.86, 0.93]
 epsilon = [0.05, 0.1, 0.15]
 fig, ax = plt.subplots()
 aspect = []
 
-labels_ap = [r'$P_{max}$: 0.79, without absorber', r'$P_{max}$: 0.86, without absorber', r'$P_{max}$: 0.93, without absorber',
-             r'$P_{max}$: 0.79, with absorber', r'$P_{max}$: 0.86, with absorber', r'$P_{max}$: 0.93, with absorber']
+labels_ap = [r'$P_{max}$: 0.93, without absorber', r'$P_{max}$: 0.86, without absorber', r'$P_{max}$: 0.79, without absorber',
+             r'$P_{max}$: 0.93, with absorber', r'$P_{max}$: 0.86, with absorber', r'$P_{max}$: 0.79, with absorber']
 
-labels_eg = [r'$\epsilon$: 0.05, without absorber', r'$\epsilon$: 0.10, without absorber', r'$\epsilon$: 0.15, without absorber',
-             r'$\epsilon$: 0.05, with absorber', r'$\epsilon$: 0.10, with absorber', r'$\epsilon$: 0.15, with absorber']
+labels_eg = [r'  : 0.15, without absorber', r'  : 0.10, without absorber', r'  : 0.05, without absorber',
+             r'  : 0.15, with absorber', r'  : 0.10, with absorber', r'  : 0.05, with absorber']
 
 colors = ['g', 'black', 'navy']
 colors_abs = ['tomato', 'orange', 'violet']
@@ -42,8 +45,8 @@ if alg == 'ap':
         pr = np.load('D:\\projects\\wamicon24\\data\\2\\pr_{}_{}.npy'.format('ap', p))
 
         aspect.append(np.max(snr))
-        plt.plot(snr, pr, label = labels_ap[i], color = colors[i], linewidth=2, linestyle='dashed')
-        plt.plot(snr_abs, pr_abs, label = labels_ap[i+3], color = colors_abs[i], linewidth=2)
+        plt.plot(snr, pr, label = labels_eg[i], color = colors[i], linewidth=2, linestyle='dashed')
+        plt.plot(snr_abs, pr_abs, label = labels_eg[i+3], color = colors_abs[i], linewidth=2)
 
 if alg == 'eg':
     for i, p in enumerate(epsilon):
@@ -53,8 +56,8 @@ if alg == 'eg':
         pr = np.load('D:\\projects\\wamicon24\\data\\2\\pr_{}_{}.npy'.format('eg', p))
 
         aspect.append(np.max(snr))
-        plt.plot(snr, pr, label = labels_eg[i], color = colors[i], linewidth=2, linestyle='dashed')
-        plt.plot(snr_abs, pr_abs, label = labels_eg[i+3], color = colors_abs[i], linewidth=2)
+        plt.plot(snr, pr, label = labels_ap[i], color = colors[i], linewidth=2, linestyle='dashed')
+        plt.plot(snr_abs, pr_abs, label = labels_ap[i+3], color = colors_abs[i], linewidth=2)
 
 if alg == 'aio':
     ap_snr = np.load('D:\\projects\\wamicon24\\data\\2\\snr_ap_0.93.npy')
@@ -73,8 +76,8 @@ if alg == 'aio':
     r_pr_abs = np.load('D:\\projects\\wamicon24\\data\\2\\pr_abs_r_xx.npy')
 
     aspect = [np.max(ap_snr), np.max(ap_snr_abs), np.max(eg_snr), np.max(eg_snr_abs)]
-    plt.plot(ap_snr, ap_pr, 'g', linewidth=2, label = '{}-greedy: without absorber'.format(r'$\epsilon$'))
-    plt.plot(ap_snr_abs, ap_pr_abs, c = 'black', linewidth=2, linestyle = (0,(5,1)), label = '{}-greedy: with absorber'.format(r'$\epsilon$'))
+    plt.plot(ap_snr, ap_pr, 'g', linewidth=2, label = '  -greedy: without absorber')
+    plt.plot(ap_snr_abs, ap_pr_abs, c = 'black', linewidth=2, linestyle = (0,(5,1)), label = '  -greedy: with absorber')
     plt.plot(eg_snr, eg_pr, c = 'navy', linestyle = (0, (1,1)), linewidth=2, label = 'AP: without absorber')
     plt.plot(eg_snr_abs, eg_pr_abs, c = 'tomato',linestyle = (5, (10,3)),  linewidth = 2, label = 'AP: with absorber')
     plt.plot(r_snr, r_pr, c = 'orange', linestyle = 'dashed', linewidth=2, label = 'Random: without absorber')
@@ -83,10 +86,11 @@ if alg == 'aio':
 
 
 # ax.set_aspect(max(aspect), adjustable='box')
-plt.grid(True, color='grey', linestyle='--', linewidth=0.3)
+plt.grid(True, color='grey', linestyle='--', linewidth=0.2)
 plt.xlabel('SNR (dB)')
 plt.ylabel('CDF of SNR')
-plt.legend(prop={'family':'Arial', 'size':12})
+# matplotlib.style.use('classic')
+plt.legend(loc='upper left', framealpha=0.3, frameon=True,prop={'family':'Arial', 'size':12})
 plt.xticks([(i)*5 for i in range(8)])
 yticks = [0.0,0.2,0.4,0.6,0.8,1.0]  # Get the current y-ticks
 plt.yticks(yticks[1:])  # Set the y-ticks to exclude the lowest one
